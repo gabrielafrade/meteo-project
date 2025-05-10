8a30e5360bd5f5f6224474ffd2d4a27484371182
Gabriela Frade <gabrielafrade@msn.com>
2025-05-10 12:20:02 +0100

    added search behaviour with api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function refreshMeteo(response) {
+  let temperatureElement = document.querySelector(
+    "#meteo-app-temperature-value"
+  );
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#meteo-app-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "3f9ec00e23oc6a139aftba624cb575a1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshMeteo);
+}
+
 function resultSubmited(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#meteo-app-city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", resultSubmited);
+searchCity("Lisbon");
